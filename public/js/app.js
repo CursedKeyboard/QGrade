81d0a622fc9d739d6cd57e8416e71a81e4ad29b4
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2021-04-26 12:49:42 -0400

    basic layout for section list

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 class Subject extends React.Component {
   render() {
     return (
+    <div>
     <div className='ui centered column centered grid'>
       <div className='column five wide'>
         <SubjectHeader
@@ -8,6 +9,12 @@ class Subject extends React.Component {
           subjectAverage={87}  
         />
         </div>
+    </div>
+      <div className='ui column centered grid'>
+        <div className='column twelve wide'>
+          <SectionList />
+        </div>
+      </div>
     </div>
     );
   }
@@ -33,11 +40,88 @@ class SubjectHeader extends React.Component {
 }
 
 class SectionList extends React.Component {
+  render () {
+    return (
+      <div className='ui centered grid segment' padding='5px'>
+        <div className="row">
+          <div className='five wide middle aligned column'>
+              <SectionHeader
+                sectionName='Section A'
+                sectionGrade={45}
+                sectionWeight={20}
+              />
+          </div>
+          <div className='five wide middle aligned column'>
+              <SectionHeader
+                sectionName='Section A'
+                sectionGrade={45}
+                sectionWeight={20}
+              />
+          </div>
+          <div className='five wide middle aligned column'>
+              <SectionHeader
+                sectionName='Section A'
+                sectionGrade={45}
+                sectionWeight={20}
+              />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='four wide middle aligned column'>
+                <SectionHeader
+                sectionName='Section A'
+                sectionGrade={45}
+                sectionWeight={20}
+              />
+          </div>
+          <div className='four wide middle aligned column'>
+                <SectionHeader
+                sectionName='Section A'
+                sectionGrade={100}
+                sectionWeight={.2}
+              />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Section extends React.Component {
 
 }
 
 class SectionHeader extends React.Component {
-
+  render () {
+    return (
+      //<div className='ui equal width grid'>
+      <div className="ui grid card">
+        <div className='middle aligned two column row'>
+          <div className='nopad middle aligned seven wide column'>
+            <div className='middle aligned ui label'>
+              <p>{this.props.sectionGrade}% | {this.props.sectionWeight}</p>
+            </div>
+            <div className='extra content bottom attached'>
+            <span className='ui'>
+              <span className='add icon'>
+                <i className='add icon'/>
+              </span>
+              <span className='edit icon'>
+                <i className='edit icon' />
+              </span>
+              <span className='trash icon'>
+                <i className='trash icon' />
+              </span>
+            </span>
+            </div>
+          </div>
+          <div className='nopad left aligned nine wide column'>
+            <h2>{this.props.sectionName}</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 class AssignmentList extends React.Component {
